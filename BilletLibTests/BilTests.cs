62a430db16639ddb353732c32b1745a31fdca63d
Lisa Vind Visby Helleberg <lisa4619@edu.easj.dk>
2017-08-30 12:04:26 +0200

    Oprettet et Class Library og tilføjet props og to metoder til klassen

---
+++ BilletLibTests/BilTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLib.Tests
+{
+    
+}
