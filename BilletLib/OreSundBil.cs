314bf5ce61b3832696fc23142ecba9af0576269c
Lisa Vind Visby Helleberg <lisa4619@edu.easj.dk>
2017-09-01 09:46:14 +0200

    Tilføjet en klasse 'OreSundBil'

---
+++ BilletLib/OreSundBil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLib
+{
+    public class OreSundBil : Køretøj
+    {
+        public override string KøretøjsType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Pris()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
