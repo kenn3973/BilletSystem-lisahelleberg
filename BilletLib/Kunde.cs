ec89663008e3a2e0f73642c7d32635475057152e
Lisa Vind Visby Helleberg <lisa4619@edu.easj.dk>
2017-09-04 12:20:11 +0200

    Opstartet kundeklasse

--- BilletLib/Kunde.cs
+++ BilletLib/Kunde.cs
@@ -6,15 +6,23 @@ using System.Threading.Tasks;
 
 namespace BilletLib
 {
-    class Kunde
+    public class Kunde
     {
-        private string navn;
+        private string Navn;
         public List<Køretøj> Ture { get; set; }
 
-        public Kunde()
+        public Kunde(String navn)
         {
+            this.Navn = navn;
             this.Ture = new List<Køretøj>();
         }
 
+        public void PrintTure()
+        {
+            foreach (var tu in Ture)
+            {
+                Console.WriteLine("{0} har passeret broen {1} gange", Navn, tu.GetAntalTureOverBro());
+            }
+        }
     }
 }
---
+++ BilletLibTests/KundeTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLib.Tests
+{
+    [TestClass()]
+    public class KundeTests
+    {
+        //// Denne test sikrer at kunderen bliver oprettet i listen og kan få printet deres ture
+        //[TestMethod()]
+        //public void PrintTureTest()
+        //{
+        //    // arrange
+        //    var kunde = new Kunde("Peter");
+        //}
+    }
+}
